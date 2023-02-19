576ea6ca9d809981df5d3968aa276ab95b9bf969
triadi <triadilaksamana@gmail.com>
2023-02-19 20:40:16 +0700

    add variables on nextjs runtime config

--- next.config.js
+++ next.config.js
@@ -10,5 +10,7 @@ module.exports = {
     publicRuntimeConfig: {
         backendUrl: process.env.API_GATEWAY_BASE_URL,
         REQUEST_TIMEOUT: process.env.REQUEST_TIMEOUT,
+        APP_MEMBERPAGE_URL: process.env.APP_MEMBERPAGE_URL,
+        API_GATEWAY_BASE_URL: process.env.API_GATEWAY_BASE_URL,
     },
 };
